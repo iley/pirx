59c3449401da42d116e86707d6478142fce60c19
Ilya Strukov <ilya@istrukov.com>
2025-06-15 15:42:49 +0100

    Continue working on code generation

--- internal/codegen/amd64_darwin/arm64_darwin.go
+++ internal/codegen/amd64_darwin/arm64_darwin.go
@@ -2,12 +2,13 @@ package amd64_darwin
 
 import (
 	_ "embed"
+	"fmt"
 	"io"
 
 	"github.com/iley/pirx/internal/parser"
 )
 
-//go:embed arm64_darwin_prologue.txt
+//go:embed prologue.txt
 var prologue string
 
 type CodegenVisitor struct {
@@ -21,14 +22,18 @@ func NewCodegenVisitor(output io.Writer) *CodegenVisitor {
 var _ parser.AstVisitor = &CodegenVisitor{}
 
 func (v *CodegenVisitor) VisitProgram(p *parser.Program) {
-	writePrologue(v.output)
+	io.WriteString(v.output, prologue)
 	for _, f := range p.Functions {
 		v.VisitFunction(f)
 	}
-	writeEpilogue(v.output)
 }
 
-func (v *CodegenVisitor) VisitFunction(f *parser.Function)                       {}
+func (v *CodegenVisitor) VisitFunction(f *parser.Function) {
+	io.WriteString(v.output, fmt.Sprintf("%s:\n", f.Name))
+
+	io.WriteString(v.output, "  ret\n")
+}
+
 func (v *CodegenVisitor) VisitParam(p *parser.Param)                             {}
 func (v *CodegenVisitor) VisitBlock(b *parser.Block)                             {}
 func (v *CodegenVisitor) VisitStatement(s parser.Statement)                      {}
@@ -36,10 +41,3 @@ func (v *CodegenVisitor) VisitLiteral(l *parser.Literal)                        
 func (v *CodegenVisitor) VisitVariableDeclaration(d *parser.VariableDeclaration) {}
 func (v *CodegenVisitor) VisitFunctionCall(f *parser.FunctionCall)               {}
 func (v *CodegenVisitor) VisitExpressionStatement(e *parser.ExpressionStatement) {}
-
-func writePrologue(output io.Writer) {
-	io.WriteString(output, prologue)
-}
-
-func writeEpilogue(output io.Writer) {
-}

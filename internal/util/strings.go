a704efac148c7d9b14a92a8821928f948c949e50
Ilya Strukov <ilya@istrukov.com>
2025-06-22 14:10:18 +0100

    Escape quotes properly, make assembly output a bit nicer

--- internal/util/strings.go
+++ internal/util/strings.go
@@ -9,7 +9,13 @@ import (
 func EscapeString(s string) string {
 	sb := strings.Builder{}
 	for _, rune := range s {
-		if unicode.IsPrint(rune) && rune < unicode.MaxASCII {
+		if rune == '"' {
+			sb.WriteString("\\\"")
+		} else if rune == '\n' {
+			sb.WriteString("\\n")
+		} else if rune == '\t' {
+			sb.WriteString("\\t")
+		} else if unicode.IsPrint(rune) && rune < unicode.MaxASCII {
 			sb.WriteRune(rune)
 		} else {
 			sb.WriteString(fmt.Sprintf("\\x%02X", rune))

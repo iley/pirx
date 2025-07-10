544423100aeaa15693b084ec9e0d243eb409ade9
Ilya Strukov <ilya@istrukov.com>
2025-07-10 18:57:18 +0100

    Minor cleanup

--- internal/checks/types.go
+++ internal/checks/types.go
@@ -38,11 +38,11 @@ func (c *TypeChecker) CheckProgram(program *ast.Program) {
 	}
 
 	for _, fn := range program.Functions {
-		c.CheckFunction(fn)
+		c.checkFunction(fn)
 	}
 }
 
-func (c *TypeChecker) CheckFunction(fn ast.Function) {
+func (c *TypeChecker) checkFunction(fn ast.Function) {
 	c.currentFunc = c.declaredFuncs[fn.Name]
 	c.hasReturn = false
 	c.declaredVars = make(map[string]types.Type)
@@ -51,7 +51,7 @@ func (c *TypeChecker) CheckFunction(fn ast.Function) {
 		c.declaredVars[arg.Name] = arg.Type
 	}
 
-	c.CheckBlock(&fn.Body)
+	c.checkBlock(&fn.Body)
 
 	// TODO: Check that each possible execution path ends with a return.
 	if c.currentFunc.ReturnType != nil && !c.hasReturn {
@@ -59,51 +59,51 @@ func (c *TypeChecker) CheckFunction(fn ast.Function) {
 	}
 }
 
-func (c *TypeChecker) CheckBlock(block *ast.Block) {
+func (c *TypeChecker) checkBlock(block *ast.Block) {
 	for _, stmt := range block.Statements {
-		c.CheckStatement(stmt)
+		c.checkStatement(stmt)
 	}
 }
 
-func (c *TypeChecker) CheckStatement(stmt ast.Statement) {
+func (c *TypeChecker) checkStatement(stmt ast.Statement) {
 	if varDecl, ok := stmt.(*ast.VariableDeclaration); ok {
-		c.CheckVariableDeclaration(varDecl)
+		c.checkVariableDeclaration(varDecl)
 	} else if exprStmt, ok := stmt.(*ast.ExpressionStatement); ok {
-		c.CheckExpressionStatement(exprStmt)
+		c.checkExpressionStatement(exprStmt)
 	} else if retStmt, ok := stmt.(*ast.ReturnStatement); ok {
-		c.CheckReturnStatement(retStmt)
+		c.checkReturnStatement(retStmt)
 	} else if ifStmt, ok := stmt.(*ast.IfStatement); ok {
-		c.CheckIfStatement(ifStmt)
+		c.checkIfStatement(ifStmt)
 	} else if whileStmt, ok := stmt.(*ast.WhileStatement); ok {
-		c.CheckWhileStatement(whileStmt)
+		c.checkWhileStatement(whileStmt)
 	} else if breakStmt, ok := stmt.(*ast.BreakStatement); ok {
-		c.CheckBreakStatement(breakStmt)
+		c.checkBreakStatement(breakStmt)
 	} else if contStmt, ok := stmt.(*ast.ContinueStatement); ok {
-		c.CheckContinueStatement(contStmt)
+		c.checkContinueStatement(contStmt)
 	} else {
 		panic(fmt.Sprintf("unsupported statement type: %v", stmt))
 	}
 }
 
-func (c *TypeChecker) CheckExpression(expr ast.Expression) types.Type {
+func (c *TypeChecker) checkExpression(expr ast.Expression) types.Type {
 	if literal, ok := expr.(*ast.Literal); ok {
-		return c.CheckLiteral(literal)
+		return c.checkLiteral(literal)
 	} else if assignment, ok := expr.(*ast.Assignment); ok {
-		return c.CheckAssignment(assignment)
+		return c.checkAssignment(assignment)
 	} else if functionCall, ok := expr.(*ast.FunctionCall); ok {
-		return c.CheckFunctionCall(functionCall)
+		return c.checkFunctionCall(functionCall)
 	} else if variableReference, ok := expr.(*ast.VariableReference); ok {
-		return c.CheckVariableReference(variableReference)
+		return c.checkVariableReference(variableReference)
 	} else if binaryOperation, ok := expr.(*ast.BinaryOperation); ok {
-		return c.CheckBinaryOperation(binaryOperation)
+		return c.checkBinaryOperation(binaryOperation)
 	} else if unaryOperation, ok := expr.(*ast.UnaryOperation); ok {
-		return c.CheckUnaryOperation(unaryOperation)
+		return c.checkUnaryOperation(unaryOperation)
 	} else {
 		panic(fmt.Sprintf("Invalid expression type: %v", expr))
 	}
 }
 
-func (c *TypeChecker) CheckLiteral(lit *ast.Literal) types.Type {
+func (c *TypeChecker) checkLiteral(lit *ast.Literal) types.Type {
 	if lit.StringValue != nil {
 		return types.String
 	} else if lit.IntValue != nil {
@@ -116,11 +116,11 @@ func (c *TypeChecker) CheckLiteral(lit *ast.Literal) types.Type {
 	panic(fmt.Sprintf("unknown literal type: %v", *lit))
 }
 
-func (c *TypeChecker) CheckVariableDeclaration(decl *ast.VariableDeclaration) {
+func (c *TypeChecker) checkVariableDeclaration(decl *ast.VariableDeclaration) {
 	c.declaredVars[decl.Name] = decl.Type
 }
 
-func (c *TypeChecker) CheckFunctionCall(call *ast.FunctionCall) types.Type {
+func (c *TypeChecker) checkFunctionCall(call *ast.FunctionCall) types.Type {
 	proto, declared := c.declaredFuncs[call.FunctionName]
 	if !declared {
 		c.errors = append(c.errors, fmt.Errorf("%d:%d: function %s is not declared", call.Loc.Line, call.Loc.Col, call.FunctionName))
@@ -131,7 +131,7 @@ func (c *TypeChecker) CheckFunctionCall(call *ast.FunctionCall) types.Type {
 	}
 
 	for i, expr := range call.Args {
-		actualArgType := c.CheckExpression(expr)
+		actualArgType := c.checkExpression(expr)
 
 		if i >= len(proto.Args) {
 			continue
@@ -147,11 +147,11 @@ func (c *TypeChecker) CheckFunctionCall(call *ast.FunctionCall) types.Type {
 	return proto.ReturnType
 }
 
-func (c *TypeChecker) CheckExpressionStatement(e *ast.ExpressionStatement) {
-	c.CheckExpression(e.Expression)
+func (c *TypeChecker) checkExpressionStatement(e *ast.ExpressionStatement) {
+	c.checkExpression(e.Expression)
 }
 
-func (c *TypeChecker) CheckAssignment(assignment *ast.Assignment) types.Type {
+func (c *TypeChecker) checkAssignment(assignment *ast.Assignment) types.Type {
 	var targetType types.Type
 	if targetVar, ok := assignment.Target.(*ast.VariableLValue); ok {
 		varName := targetVar.Name
@@ -162,7 +162,7 @@ func (c *TypeChecker) CheckAssignment(assignment *ast.Assignment) types.Type {
 			return nil
 		}
 	} else if deref, ok := assignment.Target.(*ast.DereferenceLValue); ok {
-		refType := c.CheckExpression(deref.Expression)
+		refType := c.checkExpression(deref.Expression)
 		ptrType, ok := refType.(*types.PointerType)
 		if !ok {
 			c.errors = append(c.errors, fmt.Errorf("%d:%d: dereference of a non-pointer type %s", assignment.Loc.Line, assignment.Loc.Col, refType))
@@ -173,7 +173,7 @@ func (c *TypeChecker) CheckAssignment(assignment *ast.Assignment) types.Type {
 		panic(fmt.Errorf("%d: %d: invalid lvalue %s in assignment", assignment.Loc.Line, assignment.Loc.Col, assignment.Target))
 	}
 
-	valueType := c.CheckExpression(assignment.Value)
+	valueType := c.checkExpression(assignment.Value)
 	if !valueType.Equals(targetType) {
 		c.errors = append(c.errors, fmt.Errorf("%d:%d: cannot assign value of type %s to lvalue of type %s",
 			assignment.Loc.Line,
@@ -186,7 +186,7 @@ func (c *TypeChecker) CheckAssignment(assignment *ast.Assignment) types.Type {
 	return targetType
 }
 
-func (c *TypeChecker) CheckVariableReference(ref *ast.VariableReference) types.Type {
+func (c *TypeChecker) checkVariableReference(ref *ast.VariableReference) types.Type {
 	varType, declared := c.declaredVars[ref.Name]
 	if !declared {
 		c.errors = append(c.errors, fmt.Errorf("%d:%d: variable %s is not declared before reference", ref.Loc.Line, ref.Loc.Col, ref.Name))
@@ -195,7 +195,7 @@ func (c *TypeChecker) CheckVariableReference(ref *ast.VariableReference) types.T
 	return varType
 }
 
-func (c *TypeChecker) CheckReturnStatement(stmt *ast.ReturnStatement) {
+func (c *TypeChecker) checkReturnStatement(stmt *ast.ReturnStatement) {
 	c.hasReturn = true
 
 	if stmt.Value == nil && c.currentFunc.ReturnType != nil {
@@ -205,7 +205,7 @@ func (c *TypeChecker) CheckReturnStatement(stmt *ast.ReturnStatement) {
 	}
 
 	if stmt.Value != nil {
-		typ := c.CheckExpression(stmt.Value)
+		typ := c.checkExpression(stmt.Value)
 		if c.currentFunc.ReturnType == nil {
 			c.errors = append(c.errors, fmt.Errorf("%d:%d: function %s does not have a return type but a value was provided",
 				stmt.Loc.Line, stmt.Loc.Col, c.currentFunc.Name,
@@ -218,9 +218,9 @@ func (c *TypeChecker) CheckReturnStatement(stmt *ast.ReturnStatement) {
 	}
 }
 
-func (c *TypeChecker) CheckBinaryOperation(binOp *ast.BinaryOperation) types.Type {
-	leftType := c.CheckExpression(binOp.Left)
-	rightType := c.CheckExpression(binOp.Right)
+func (c *TypeChecker) checkBinaryOperation(binOp *ast.BinaryOperation) types.Type {
+	leftType := c.checkExpression(binOp.Left)
+	rightType := c.checkExpression(binOp.Right)
 	resultType, ok := binaryOperationResult(binOp.Operator, leftType, rightType)
 	if !ok {
 		c.errors = append(c.errors, fmt.Errorf("%d:%d: binary operation %s cannot be applied to values of types %s and %s",
@@ -234,8 +234,8 @@ func (c *TypeChecker) CheckBinaryOperation(binOp *ast.BinaryOperation) types.Typ
 	return resultType
 }
 
-func (c *TypeChecker) CheckUnaryOperation(unaryOp *ast.UnaryOperation) types.Type {
-	operandType := c.CheckExpression(unaryOp.Operand)
+func (c *TypeChecker) checkUnaryOperation(unaryOp *ast.UnaryOperation) types.Type {
+	operandType := c.checkExpression(unaryOp.Operand)
 	resultType, ok := unaryOperationResult(unaryOp.Operator, operandType)
 	if !ok {
 		c.errors = append(c.errors, fmt.Errorf("%d:%d: unary operation %s cannot be applied to a value of type %s",
@@ -248,8 +248,8 @@ func (c *TypeChecker) CheckUnaryOperation(unaryOp *ast.UnaryOperation) types.Typ
 	return resultType
 }
 
-func (c *TypeChecker) CheckIfStatement(stmt *ast.IfStatement) {
-	exprType := c.CheckExpression(stmt.Condition)
+func (c *TypeChecker) checkIfStatement(stmt *ast.IfStatement) {
+	exprType := c.checkExpression(stmt.Condition)
 	if exprType != types.Bool {
 		c.errors = append(c.errors, fmt.Errorf("%d:%d: expected an expression of type bool in if condition, got type %s",
 			stmt.Loc.Line,
@@ -257,14 +257,14 @@ func (c *TypeChecker) CheckIfStatement(stmt *ast.IfStatement) {
 			exprType,
 		))
 	}
-	c.CheckBlock(&stmt.ThenBlock)
+	c.checkBlock(&stmt.ThenBlock)
 	if stmt.ElseBlock != nil {
-		c.CheckBlock(stmt.ElseBlock)
+		c.checkBlock(stmt.ElseBlock)
 	}
 }
 
-func (c *TypeChecker) CheckWhileStatement(stmt *ast.WhileStatement) {
-	exprType := c.CheckExpression(stmt.Condition)
+func (c *TypeChecker) checkWhileStatement(stmt *ast.WhileStatement) {
+	exprType := c.checkExpression(stmt.Condition)
 	if exprType != types.Bool {
 		c.errors = append(c.errors, fmt.Errorf("%d:%d: expected an expression of type bool in while condition, got type %s",
 			stmt.Loc.Line,
@@ -272,14 +272,14 @@ func (c *TypeChecker) CheckWhileStatement(stmt *ast.WhileStatement) {
 			exprType,
 		))
 	}
-	c.CheckBlock(&stmt.Body)
+	c.checkBlock(&stmt.Body)
 }
 
-func (c *TypeChecker) CheckBreakStatement(stmt *ast.BreakStatement) {
+func (c *TypeChecker) checkBreakStatement(stmt *ast.BreakStatement) {
 	// noop
 }
 
-func (c *TypeChecker) CheckContinueStatement(stmt *ast.ContinueStatement) {
+func (c *TypeChecker) checkContinueStatement(stmt *ast.ContinueStatement) {
 	// noop
 }
 

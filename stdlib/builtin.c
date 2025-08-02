0bf52272a3454ffb014e01b364fa263820acbed2
Ilya Strukov <ilya@istrukov.com>
2025-08-02 13:25:35 +0300

    Test slices of pointers

--- stdlib/builtin.c
+++ stdlib/builtin.c
@@ -3,26 +3,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void *Pirx_Alloc(int size) { return malloc(size); }
+void *Pirx_Alloc(int size) { return calloc(size, 1); }
 
-void Pirx_Dispose(void *ptr) {
-  // printf("*** disposing %p\n", ptr);
-  free(ptr);
-}
+void Pirx_Dispose(void *ptr) { free(ptr); }
 
 Pirx_Slice Pirx_Slice_Alloc(int elem_size, int size, int cap) {
-  // printf("*** allocating slice: elem_size=%d, size=%d, cap=%d\n", elem_size, size, cap);
-  // TODO: Validate size and cap.
   Pirx_Slice slice = {
       .data = Pirx_Alloc(elem_size * cap),
       .size = size,
       .cap = cap,
   };
-  // printf("*** allocated %p\n", slice.data);
   return slice;
 }
 
-void Pirx_Slice_Dispose(Pirx_Slice slice) {
-  // printf("*** disposing slice data=%p, size=%d, cap=%d\n", slice.data, slice.size, slice.cap);
-  Pirx_Dispose(slice.data);
-}
+void Pirx_Slice_Dispose(Pirx_Slice slice) { Pirx_Dispose(slice.data); }
